2c271511832ab309bae295531f017f9be471287a
Vastuolu <muhamadnaufalnabillansyah@gmail.com>
2026-03-15 04:50:55 +0700

    second commit (implementasi teknik table-driven construction)

---
+++ KodePos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class KodePos
+{
+	private static Dictionary<string, int> kodeKelurahan = new Dictionary<string, int>
+		{
+			{"Batununggal",40266},
+			{"Kujangsari", 40287},
+			{"Mengger", 40267},
+			{"Wates", 40256},
+			{"Cijaura", 40287},
+			{"Jatisari", 40286},
+			{"Margasari", 40286},
+			{"Sekejati", 40286},
+			{"Kebonwaru", 40272},
+			{"Maleer", 40274}
+		};
+
+	public int getKodePos(string kelurahan)
+	{
+		int kodePos = 0;
+		if (kodeKelurahan.TryGetValue(kelurahan, out kodePos))
+		{
+			return kodePos;
+		}
+		else
+		{
+			Console.WriteLine("Keluaqhan tidak ditemukan.");
+			return -1;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+       public static void Main()
+    {
+        Console.WriteLine("++Teknik Table Drive Construction++");
+        Console.Write("Masukkan Kelurahan: ");
+        string kelurahan = Console.ReadLine();
+        int kodePos = new KodePos().getKodePos(kelurahan);
+        Console.WriteLine("Kode Pos Kelurahan " + kelurahan + ": "+kodePos);
+    }
+}
